2b1cf2a82914b839c81fa50bc66a31ced78b825a
evan <stranddavis@gmail.com>
2017-11-05 11:26:56 -0600

    add momentjs and tweak time stamps

---
+++ playground/time.js
@@ -0,0 +1,19 @@
+let moment = require("moment");
+
+/* let date = moment();
+
+let timestamp = date.format("MMM Do YYYY");
+
+console.log(timestamp);*/
+
+let date = moment();
+
+let timestamp = date.valueOf();
+
+//let timestamp = date.format("h:mma");
+
+
+console.log(timestamp);
+
+
+
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+let moment = require("moment");
+
 let generateMessage = (from, text) => {
 	return {
 		from,
 		text,
-		createdAt: new Date().getTime()
+		createdAt: moment().valueOf()
 	}
 };
 
@@ -11,7 +13,7 @@ let generateLocationMessage = (from, lat, lon) => {
 	return {
 		from,
 		url: `https://www.google.com/maps?q=${lat},${lon}`,
-		createdAt: new Date().getTime()
+		createdAt: moment().valueOf()
 	};
 }
 
